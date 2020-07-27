2d140c4b5c72d981c200b30e6b430690df6c3b9f
Victor <victor.tran@tempestas.io>
2020-07-27 18:44:19 -0400

    complete moving_zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,11 +2,19 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
+def moving_zeroes(arr: list):
     # Your code here
+    # I'd probably use a linked list here but iterate through list and add to tail or just use list methods
+    start = 0
+    popped = 0
+    while start + popped != len(arr):
+        if arr[start] == 0:
+            arr.append(arr.pop(start))
+            popped += 1
+        else:
+            start += 1
 
-    pass
-
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation

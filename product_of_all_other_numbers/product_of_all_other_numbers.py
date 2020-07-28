b9f23d3db5e46bbc867551262b07e75a3af46fa6
Victor <victor.tran@tempestas.io>
2020-07-28 02:15:55 -0400

    finish

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,10 +2,23 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+from functools import reduce
+
+def partition_list_without_i(arr, i):
+    return arr[:i] + arr[i+1:]
+
 def product_of_all_other_numbers(arr):
     # Your code here
-
-    pass
+    # given an item at index you would multiply that item by all other items
+    new_list = []
+    for i in range(len(arr)):
+        # grab the beginning up to i [:i] i being exclusive
+        # grab from i + 1 to end
+        # reduce by multiplying
+        other_numbers_array = partition_list_without_i(arr, i)
+        reduced_product = reduce(lambda prev, next_value: prev * next_value, other_numbers_array)
+        new_list.append(reduced_product)
+    return new_list
 
 
 if __name__ == '__main__':
